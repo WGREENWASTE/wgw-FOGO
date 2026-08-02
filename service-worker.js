7e3e357f27157e4b64b6e0e9a329ea661f2b5d7b
WGREENWASTE <sht6@hotmail.com>
2026-08-02 21:41:10 +1000

    Version 2.0 - Bin serial number lookup

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,5 @@
-
-const CACHE="wgw-fogo-v4";
-  const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='wgw-fogo-v2-0';
+const ASSETS=['./','./index.html','./manifest.json','./service-worker.js','./bin-data.js','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))));});
