7b205d8c1a133348109922d4bfc65c4835596b1c
WGREENWASTE <sht6@hotmail.com>
2026-08-03 09:54:31 +1000

    Version 2.02 -  FO serial number support

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='wgw-fogo-v2-0';
+const CACHE='wgw-fogo-v2-02';
 const ASSETS=['./','./index.html','./manifest.json','./service-worker.js','./bin-data.js','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
